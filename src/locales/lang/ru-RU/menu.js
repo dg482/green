7a70c2bb5d70c0df1939f982e922972cad9b5078
Andrey Martynov <a.martinov@ddemo.ru>
2021-03-19 19:31:44 +0500

    change route, locale, delete default views

---
+++ src/locales/lang/ru-RU/menu.js
@@ -0,0 +1,11 @@
+export default {
+    'menu.welcome': 'Добро пожаловать',
+    'menu.home': 'FastDog',
+    'menu.dashboard': 'Администрирование',
+    'menu.dashboard.workplace': 'Рабочий стол',
+    'menu.exception': 'Exception',
+    'menu.exception.not-permission': '403',
+    'menu.exception.not-find': '404',
+    'menu.exception.server-error': '500',
+    'menu.exception.trigger': 'Trigger'
+}
