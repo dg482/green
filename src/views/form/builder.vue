bf9886ba2873bcb9ff5f66120cbd61fcdcf3aa56
dg <d.g.dev482@gmail.com>
2021-03-24 22:23:21 +0300

    dev

---
+++ src/views/form/builder.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page-header-index-wide page-header-wrapper-grid-content-main">
+    <a-row :gutter="[24,16]">
+      <a-col :md="24" :lg="24">
+        <page-header-wrapper/>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Builder',
+  components: {},
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
